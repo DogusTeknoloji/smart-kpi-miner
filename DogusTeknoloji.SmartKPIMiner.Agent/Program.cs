cd8ee6340721024a9aea855b723a9f63a92fef7c
Onur Akkaya <onur.akkaya@d-teknoloji.com.tr>
2020-12-29 03:20:12 +0300

    Thread.Timer management moved to constructor

--- DogusTeknoloji.SmartKPIMiner.Agent/Program.cs
+++ DogusTeknoloji.SmartKPIMiner.Agent/Program.cs
@@ -6,12 +6,7 @@ namespace DogusTeknoloji.SmartKPIMiner.Agent
         public static void Main(string[] args)
         {
 #if DEBUG
-            using (SmartKPIMinerAgent minerAgent = new SmartKPIMinerAgent())
-            {
-                System.Threading.Tasks.Task t = minerAgent.KPIProcessAsync();
-                System.Threading.Tasks.Task tLog = minerAgent.DebugLogAsync();
-                System.Threading.Tasks.Task.WaitAll(new[] { t, tLog });
-            }
+            SmartKPIMinerAgent minerAgent = new SmartKPIMinerAgent();
 #elif RELEASE
             ServiceBase.Run(new SmartKPIMinerAgent());
 #endif
--- DogusTeknoloji.SmartKPIMiner.Agent/SmartKPIMinerAgent.cs
+++ DogusTeknoloji.SmartKPIMiner.Agent/SmartKPIMinerAgent.cs
@@ -11,55 +11,50 @@ namespace DogusTeknoloji.SmartKPIMiner.Agent
         protected Timer _mainServiceTimer;
         protected Timer _loggingTimer;
         protected OperationContext context = new OperationContext();
+        protected long _mainSvcDueTime = 0, _mainSvcPeriod = 0;
+        protected long _logSvcDueTime = 0, _logSvcPeriod = 0;
 
         public SmartKPIMinerAgent()
         {
+            this._mainSvcDueTime = 0;
+            this._mainSvcPeriod = (long)TimeSpan.FromMinutes(15).TotalMilliseconds;
 
-        }
-        public Task DebugLogAsync()
-        {
-            _loggingTimer = new Timer(callback: state => context.LogManager.ProcessLogQueue(), state: null, dueTime: 0, period: (long)TimeSpan.FromSeconds(10).TotalMilliseconds);
-            return Task.CompletedTask;
+            this._logSvcDueTime = 0;
+            this._logSvcPeriod = (long)TimeSpan.FromSeconds(10).TotalMilliseconds;
+
+            _loggingTimer = new Timer(callback: state => LogProcessAsync(), state: null, dueTime: Timeout.Infinite, period: Timeout.Infinite);
+            _mainServiceTimer = new Timer(callback: async state => await KPIProcessAsync(), state: null, dueTime: Timeout.Infinite, period: Timeout.Infinite);
         }
 
         public void LogProcessAsync()
         {
-            _loggingTimer?.Change(dueTime: Timeout.Infinite, period: 0);
+            _loggingTimer?.Change(dueTime: Timeout.Infinite, period: Timeout.Infinite);
             context.LogManager.ProcessLogQueue();
-            _loggingTimer?.Change(dueTime: 0, period: (long)TimeSpan.FromSeconds(10).TotalMilliseconds);
+            _loggingTimer?.Change(dueTime: _logSvcDueTime, period: _logSvcPeriod);
         }
 
         public async Task KPIProcessAsync()
         {
-            _mainServiceTimer?.Change(dueTime: Timeout.Infinite, period: 0);
+            _mainServiceTimer?.Change(dueTime: Timeout.Infinite, period: Timeout.Infinite);
             ServiceManager.Initialize();
             await context.ProcessItemsAsync();
-            _mainServiceTimer?.Change(dueTime: 0, period: (long)TimeSpan.FromMinutes(15).TotalMilliseconds);
+            _mainServiceTimer?.Change(dueTime: _mainSvcDueTime, period: _mainSvcPeriod);
         }
 
         protected override void OnStart(string[] args)
         {
             base.OnStart(args);
-            _loggingTimer = new Timer(callback: state => LogProcessAsync(), state: null, dueTime: 0, period: (long)TimeSpan.FromSeconds(10).TotalMilliseconds);
-            _mainServiceTimer = new Timer(callback: async state => await KPIProcessAsync(), state: null, dueTime: 0, period: (long)TimeSpan.FromMinutes(15).TotalMilliseconds);
-            WaitCallback();
-        }
-
-        private async Task WaitCallback()
-        {
-            while (true)
-            {
-                Thread.Sleep(Timeout.InfiniteTimeSpan);
-            }
+            _mainServiceTimer?.Change(dueTime: _mainSvcDueTime, period: _mainSvcPeriod);
+            _loggingTimer?.Change(dueTime: _logSvcDueTime, period: _logSvcPeriod);
         }
 
         protected override void OnStop()
         {
             base.OnStop();
-            _mainServiceTimer?.Change(dueTime: Timeout.Infinite, period: 0);
+            _mainServiceTimer?.Change(dueTime: Timeout.Infinite, period: Timeout.Infinite);
             _mainServiceTimer.DisposeAsync();
-            _loggingTimer?.Change(dueTime: Timeout.Infinite, period: 0);
-            _loggingTimer.Dispose();
+            _loggingTimer?.Change(dueTime: Timeout.Infinite, period: Timeout.Infinite);
+            _loggingTimer.DisposeAsync();
         }
     }
 }
